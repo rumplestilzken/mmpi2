774f147fb2739f865b7d633012029e0e04faf52e
rumplestilzken <rumplestilzken@gmail.com>
2023-11-28 13:26:36 -0500

    Continued scales

--- src/main/java/com/rumplestilzken/mmpi2/data/scale/Sc6.java
+++ src/main/java/com/rumplestilzken/mmpi2/data/scale/Sc6.java
@@ -1,5 +1,7 @@
 package com.rumplestilzken.mmpi2.data.scale;
 
+import java.util.List;
+
 public class Sc6 extends Scale {
     @Override
     public String getDescription() {
@@ -9,5 +11,52 @@ public class Sc6 extends Scale {
     @Override
     public String toString() {
         return "Sc6";
+    }
+
+
+    @Override
+    public List<String> getTrueQuestions() {
+        List<String> list = super.getTrueQuestions();
+        list.add("131");
+        list.add("227");
+        list.add("248");
+        list.add("250");
+        list.add("269");
+        return list;
+    }
+
+    @Override
+    public List<String> getFalseQuestions() {
+        List<String> list = super.getFalseQuestions();
+        list.add("263");
+        return list;
+    }
+
+    @Override
+    public List<String> getMaleTScale() {
+        List<String> list = super.getMaleTScale();
+        list.add("");
+        list.add("35");
+        list.add("42");
+        list.add("50");
+        list.add("58");
+        list.add("66");
+        list.add("74");
+        list.add("81");
+        return list;
+    }
+
+    @Override
+    public List<String> getFemaleTScale() {
+        List<String> list = super.getFemaleTScale();
+        list.add("");
+        list.add("37");
+        list.add("45");
+        list.add("54");
+        list.add("62");
+        list.add("70");
+        list.add("79");
+        list.add("87");
+        return list;
     }
 }
