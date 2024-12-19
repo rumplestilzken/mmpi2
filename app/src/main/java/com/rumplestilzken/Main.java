fb30fd512fa81e87544be8967907e15c9f3b459a
rumplestilzken <rumplestilzken@gmail.com>
2024-12-19 12:31:01 -0500

    Beginning work on UI

--- app/src/main/java/com/rumplestilzken/Main.java
+++ app/src/main/java/com/rumplestilzken/Main.java
@@ -1,6 +1,21 @@
 package com.rumplestilzken;
-class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main_window.fxml"));
+        primaryStage.setTitle("MMPI-2");
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.show();
     }
 }
--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module mmpi.app.main {
+    requires javafx.controls;
+    requires javafx.fxml;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires itextpdf;
     requires org.json;
+
+    opens com.rumplestilzken to javafx.fxml;
+
+    exports com.rumplestilzken;
 }
